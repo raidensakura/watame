16943bf4f72a6206727a8fe90ff8da2753e8efba
Shoukaku <mary.tateyama@gmail.com>
2020-05-20 03:56:06 +0800

    some stuf

--- commands/faction.js
+++ commands/faction.js
@@ -1,4 +1,7 @@
 const quiz = require('./quiz.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const guild = new Discord.Guild();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('database', 'user', 'password', {
     host: 'localhost',
@@ -11,7 +14,7 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 module.exports = {
     name: 'faction',
     description: 'Quiz-based role assignment.',
-    //DMOnly: true,
+    DMOnly: true,
     execute(message, args) {
         const Tags = sequelize.define('faction', {
             uid: {
@@ -27,12 +30,13 @@ module.exports = {
         //do stuff here
         var increment = 0;
         var points = 0;
+        var authorUID = "";
 
         function askQuestion() {
             const filter = response => {
                 return quiz[increment].answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
             };
-    
+
             message.channel.send(quiz[increment].question).then(() => {
                 var answered = 'no';
                 message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
@@ -46,25 +50,47 @@ module.exports = {
                         var answerIndex = quiz[increment].answers.indexOf(collected.first().content);
                         points = points + quiz[increment].points[answerIndex];
 
-                        console.log("you now have" + points);
+                        console.log(`${collected.first().author.tag} now have ${points} points`);
                         answered = 'yes';
                         increment++;
+                        authorUID = `${collected.first().author}`;
 
                         if (increment == quiz.length) {
                             message.channel.send('Quiz completed.');
+                            saveScore(authorUID, points);
                             return;
                         }
                         if (answered == 'yes') askQuestion();
                     })
                     .catch(collected => {
                         message.channel.send('Quiz exited.');
-                        console.log(collected);
+                        console.log(`${collected.first().author.tag} timed out.`);
                         return;
                     });
             });
         }
 
-        
+        async function saveScore(authorUID, points) {
+            try {
+                // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+                const tag = await Tags.create({
+                    uid: authorUID,
+                    score: points,
+                });
+                console.log(`Tag added.`);
+            }
+            catch (e) {
+                if (e.name === 'SequelizeUniqueConstraintError') {
+                    console.log('That tag already exists. Trying to update existing record...');
+
+                    const affectedRows = await Tags.update({ score: points }, { where: { uid: authorUID } });
+                    if (affectedRows > 0) {
+                        return console.log(`Tag was edited.`);
+                    }
+                    return console.log(`Could not find a tag with name.`);
+                }
+            }
+        }
         askQuestion();
         Tags.sync();
     },
