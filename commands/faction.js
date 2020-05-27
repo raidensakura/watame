c1a51e82d7528fa30b58949d1b185415bb1801e2
Shoukaku <mary.tateyama@gmail.com>
2020-05-28 00:28:56 +0800

    Fix function call logic
    Related to saveScore and giveRole not executing correctly, also an update to the default quiz file.

--- commands/faction.js
+++ commands/faction.js
@@ -12,6 +12,7 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 
 module.exports = {
     name: 'faction',
+    aliases: ['factions', 'quiz', 'quizzes'],
     description: 'Quiz-based role assignment for Sleeping Knights.',
     DMOnly: true,
     cooldown: 15,
@@ -28,6 +29,7 @@ module.exports = {
             },
         });
         let server, member, increment = 0, points = 0;
+        let sereGamersPoints = 580, sereAxisPoints = 520; //these are differetiating points of the two factions
 
         async function fetchServer() {
             console.log(`${message.author.tag} started quiz command.`);
@@ -44,19 +46,21 @@ module.exports = {
         async function checkRole() {
             let TimeoutMessage = 'true';
             if (member.roles.cache.some(role => role.id === sereGamersID || role.id === sereAxisID)) {
-                message.author.send("Seems like you already have a faction role. Reset? `yes`/`no`");
-                const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { maxMatches: 1, time: 10000 });
+                message.author.send("Seems like you already have a faction role. Reset? `yes` or `no`");
+                const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id &&
+                    (m.content.toLowerCase() === 'yes' || m.content.toLowerCase() === 'no'), { max: 1, time: 30000 });
                 collector.on('collect', message => {
                     if (message.content.toLowerCase() == "yes") {
                         try {
                             TimeoutMessage = 'false';
                             member.roles.remove(sereGamersID);
                             member.roles.remove(sereAxisID);
-                            console.log(`Removed faction role(s) from ${message.author.tag}`);
+                            console.log(`Removed faction role(s) from ${message.author.tag}.`);
                             message.channel.send("Your faction role was reset.");
                         } catch (error) {
                             console.log(error);
                             console.log(`There was an error removing faction role for ${message.author.tag}.`);
+                            message.channel.send("Error trying to remove your role.");
                         }
                         askQuestion();
                     } else if (message.content.toLowerCase() == "no") {
@@ -68,7 +72,7 @@ module.exports = {
                 collector.on('end', collected => {
                     if (TimeoutMessage == 'true') {
                         console.log(`${message.author.tag} timed out on role removal prompt.`);
-                        message.channel.send("Prompt timed out.");
+                        message.channel.send("Invalid response given / prompt timed out.");
                     }
                 });
             } else {
@@ -82,13 +86,13 @@ module.exports = {
                     || (response.content.toLowerCase() === 'abort')) && (response.author.id === message.author.id));
             };
 
-            message.channel.send(quiz[increment].question).then(() => {
+            message.author.send(quiz[increment].question).then(() => {
                 let answered = 'no';
                 message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
                     .then(collected => {
                         if (collected.first().content === 'abort') {
-                            message.channel.send('Quiz aborted.');
                             console.log(`${message.author.tag} aborted quiz.`);
+                            message.channel.send('Quiz aborted.');
                             return;
                         }
 
@@ -99,15 +103,17 @@ module.exports = {
                         answered = 'yes'; increment++;
 
                         if (increment == quiz.length) {
+                            answered = 'finished';
+                            console.log(`${collected.first().author.tag} answered all questions.`);
                             message.channel.send('Quiz completed.');
-                            saveScore(collected.first().author, points);
-                            return;
+                            saveScore(`${collected.first().author}`, points);
+                            return giveRole();
                         }
                         if (answered == 'yes') askQuestion();
                     })
                     .catch(collected => {
-                        message.channel.send('Quiz timed out.');
-                        return console.log(`${message.author.tag} timed out from quiz.`);
+                        console.log(`${message.author.tag} timed out from quiz.`);
+                        return message.channel.send('Quiz timed out.');
                     });
             });
         }
@@ -131,11 +137,11 @@ module.exports = {
                     return console.log(`Could not find tag for ${message.author.tag}.`);
                 }
             }
-            Tags.sync(); giveRole();
+            Tags.sync();
         }
 
         async function giveRole() {
-            if (points >= 600) {
+            if (points >= sereGamersPoints) {
                 try {
                     await member.roles.add(sereGamersID);
                     await message.channel.send('You have been awarded `SereGamers` role.');
@@ -145,7 +151,7 @@ module.exports = {
                     console.log(`There was an error trying to give SereGamers role to ${message.author.tag}.`);
                     message.channel.send('Error trying to give SereGamers role.');
                 }
-            } else if (points <= 400) {
+            } else if (points <= sereAxisPoints) {
                 try {
                     await member.roles.add(sereAxisID);
                     await message.channel.send('You have been awarded `SereAxis` role.');
@@ -155,9 +161,14 @@ module.exports = {
                     console.log(`There was an error trying to give SereAxis role to ${message.author.tag}.`);
                     message.channel.send('Error trying to give SereGamers role.');
                 }
-            } else {
+            } else if (points > sereAxisPoints && points < sereGamersPoints) {
+                console.log(`${message.author.tag} ended up being neutral.`)
                 return message.channel.send('Seems like you live in between of both factions.\
                 \nPerfectly balanced, as all things should be.');
+            } else {
+                console.log(`There's something wrong with ${message.author.tag}'s points: ${points}`)
+                return message.channel.send('Something went wrong calculating your score.\
+                \nPlease tell my owner about this, thank you!');
             }
         }
         fetchServer();
