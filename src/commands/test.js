5cfb050473fd040005aba72f0ee838cca33acfaa
Shoukaku <mary.tateyama@gmail.com>
2020-07-15 20:54:44 +0800

    Logic and syntax change

--- src/commands/test.js
+++ src/commands/test.js
@@ -1,27 +1,17 @@
+// Just a boilerplate command to list all the properties
 module.exports = {
 	name: 'test',
 	description: 'What are you even trying to test?',
-	usage: '<what is this>',
+	usage: '<this does nothing>',
 	aliases: ['testing'],
-	// cooldown amount in seconds
 	cooldown: 5,
-	// if command is server-only
 	guildOnly: false,
-	// if command is DM only
 	DMOnly: false,
-	// if command is for bot owner only
 	ownerOnly: false,
-	// if command is for guild staff only
 	staffOnly: false,
-	// if command require database
 	requireTag: false,
-	// if command require argument
 	args: false,
 	async execute(client, message) {
-		let response = await client.awaitReply(message, "what are you testing, exactly?");
-		if (response) {
-			let replies = ["I see...", "Okay then...", "Weird, but okay..."];
-			message.channel.send(`${response}? ${replies[Math.floor(Math.random() * replies.length)]}`);
-		} else return;
+		message.channel.send('This does nothing!');
 	},
 };
