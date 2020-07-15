839a140b5e7e7da65574394027fbacf5469dea4f
Shoukaku <mary.tateyama@gmail.com>
2020-07-16 04:25:10 +0800

    Production (#14) (#15)
    * Install eslint and babel
    
    * Badge update
    
    * Move files to src
    
    * Readme update

--- src/commands/test.js
+++ src/commands/test.js
@@ -1,17 +1,27 @@
-// Just a boilerplate command to list all the properties
 module.exports = {
 	name: 'test',
 	description: 'What are you even trying to test?',
-	usage: '<this does nothing>',
+	usage: '<what is this>',
 	aliases: ['testing'],
+	// cooldown amount in seconds
 	cooldown: 5,
+	// if command is server-only
 	guildOnly: false,
+	// if command is DM only
 	DMOnly: false,
+	// if command is for bot owner only
 	ownerOnly: false,
+	// if command is for guild staff only
 	staffOnly: false,
+	// if command require database
 	requireTag: false,
+	// if command require argument
 	args: false,
 	async execute(client, message) {
-		message.channel.send('This does nothing!');
+		let response = await client.awaitReply(message, "what are you testing, exactly?");
+		if (response) {
+			let replies = ["I see...", "Okay then...", "Weird, but okay..."];
+			message.channel.send(`${response}? ${replies[Math.floor(Math.random() * replies.length)]}`);
+		} else return;
 	},
 };
