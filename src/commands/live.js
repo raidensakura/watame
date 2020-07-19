dd262cbd10677cf547a7c5b3a9ead7de1bb13105
Shoukaku <mary.tateyama@gmail.com>
2020-07-20 00:04:53 +0800

    Update live.js

--- src/commands/live.js
+++ src/commands/live.js
@@ -7,36 +7,63 @@ const ms = require("ms");
 module.exports = {
 	name: 'live',
 	description: 'Check channel stats for the hololive Vtubers',
-	usage: '<name>',
+	usage: '<name/channel ID>',
 	aliases: ['hololive'],
 	cooldown: 5,
 	guildOnly: true,
-	args: false,
 	async execute(client, message, args) {
 
 		if (!youtubeAPIKey || youtubeAPIKey === 'key_here')
 			return message.channel.send("YouTube API Key is missing from config file...");
 
-		let channel;
-		if (args[0] === 'watame') channel = await youtube.getChannel('UCqm3BQLlJfvkTsX_hvm0UmA');
-		if (args[0] === 'fubuki') channel = await youtube.getChannel('UCdn5BQ06XqgXoAxIhbqw5Rg');
-		if (args[0] === 'aqua') channel = await youtube.getChannel('UC1opHUrw8rvnsadT-iGp7Cg');
-		if (args[0] === 'haato') channel = await youtube.getChannel('UC1CfXB_kRs3C-zaeTG3oGyg');
-		if (args[0] === 'matsuri') channel = await youtube.getChannel('UCQ0UDLQCjY0rmuxCDE38FGg');
-		if (args[0] === 'miko') channel = await youtube.getChannel('UC-hM6YJuNYVAmUWxeIr9FeA');
-		if (args[0] === 'mio') channel = await youtube.getChannel('UCp-5t9SrOQwXMU7iIjQfARg');
-		if (args[0] === 'ayame') channel = await youtube.getChannel('UC7fk0CB07ly8oSl0aqKkqFg');
-		if (args[0] === 'sora') channel = await youtube.getChannel('UCp6993wxpyDPHUpavwDFqgg');
-		else channel = await youtube.getChannel('UCJFZiqLMntJufDCHc6bQixg');
-		// console.log(channel);
+		const name = {
+			"watame": "UCqm3BQLlJfvkTsX_hvm0UmA",
+			"aqua": "UC1opHUrw8rvnsadT-iGp7Cg",
+			"haato": "UC1CfXB_kRs3C-zaeTG3oGyg",
+			"matsuri": "UCQ0UDLQCjY0rmuxCDE38FGg",
+			"miko": "UC-hM6YJuNYVAmUWxeIr9FeA",
+			"mio": "UCp-5t9SrOQwXMU7iIjQfARg",
+			"ayame": "UC7fk0CB07ly8oSl0aqKkqFg",
+			"sora": "UCp6993wxpyDPHUpavwDFqgg",
+		};
 
-		const embed = new Discord.MessageEmbed()
-			.setColor('#F47FFF')
-			.setThumbnail(channel.profilePictures.medium.url)
-			.setAuthor(channel.name, channel.profilePictures.default.url, channel.url)
-			.addField('Subscribers', channel.subCount)
-			.addField('Views', channel.views)
-			.addField('Date Created', `${ms(Date.now() - Date.parse(channel.dateCreated), { long: true })} ago`)
-		message.channel.send(embed);
+		if (!args[0]) {
+			let response = await client.awaitReply(message, 'Enter the Username, URL or ID of the channel you wish to display');
+			return getChannel(response);
+		}
+
+		if (args[0] === 'debug') {
+			let search = await youtube.searchChannels('PewDiePie', 1);
+			console.log(search);
+			return message.channel.send('Debug mode.');
+		}
+
+		if (name[args[0].toLowerCase()]) {
+			return getChannel(name[args[0].toLowerCase()]);
+		} else {
+			message.reply('There is no hololive channel under that name');
+		}
+
+		async function getChannel(id) {
+			try {
+				let channel = await youtube.getChannel(id);
+				sendEmbed(channel);
+			} catch {
+				message.reply('No channel was found under that ID');
+			}
+		}
+
+		async function sendEmbed(channel) {
+			const embed = new Discord.MessageEmbed()
+				.setColor('#F47FFF')
+				.setThumbnail(channel.profilePictures.medium.url)
+				.setAuthor(channel.name, channel.profilePictures.default.url, channel.url)
+				.addField('Subscribers', channel.subCount.toLocaleString())
+				.addField('Total Views', channel.views.toLocaleString())
+				.addField('Channel Creation', `${ms(Date.now() - Date.parse(channel.dateCreated), { long: true })} ago`)
+			await message.channel.send(embed);
+		}
+
+		// console.log(channel);
 	},
 };
--- src/modules/functions.js
+++ src/modules/functions.js
@@ -9,11 +9,13 @@ module.exports = (client) => {
     */
 	client.awaitReply = async (msg, question, limit = 60000) => {
 		const filter = m => m.author.id === msg.author.id;
-		await msg.channel.send(question);
+		let prompt = await msg.channel.send(question);
 		try {
 			const collected = await msg.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] });
+			await prompt.delete()
 			return collected.first().content;
 		} catch (e) {
+			await prompt.delete()
 			return false;
 		}
 	};
