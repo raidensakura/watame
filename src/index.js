6d121cbb54ddfc6a781693630efcc2261e9a757d
Shoukaku <mary.tateyama@gmail.com>
2020-09-22 20:57:38 +0800

    wao

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { TOKEN } = require('./data/config.js');
+const { TOKEN } = require('./data/config.js'); //wao
 
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
