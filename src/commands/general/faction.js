4199442a668ad14ee566dced3491487ef42af0aa
Shoukaku <mary.tateyama@gmail.com>
2020-09-25 19:23:18 +0800

    Improve code readibility

--- src/commands/general/faction.js
+++ src/commands/general/faction.js
@@ -7,9 +7,8 @@ const { BOT_URL } = require('../../data/config.js');
 
 const quiz = require('../../data/quiz.json');
 
-const factionModel = require('../../data/models/Faction.js');
-
 const EmbedGenerator = require('../../modules/sendEmbed');
+
 const ms = require('ms');
 
 module.exports = {
@@ -19,7 +18,6 @@ module.exports = {
 	DMOnly: true,
 	cooldown: 5,
 	execute(client, message) {
-
 		let server, member;
 		async function fetchServer() {
 			try {
@@ -32,9 +30,8 @@ module.exports = {
 				}
 
 				checkRole();
-			} catch (error) {
-				client.logger.error(`On fetchServer() for ${message.author.tag}: ${error}`);
-				return message.reply('Error trying to fetch server info.');
+			} catch (e) {
+				return client.logger.error(`Error fetching server for ${message.author.tag}`, e);
 			}
 		}
 
@@ -59,8 +56,8 @@ module.exports = {
 					return message.channel.send('Invalid reply, please run the command again.');
 				}
 
-			} catch (error) {
-				client.logger.error(`On CheckRole() for ${message.author.tag}: ${error}`);
+			} catch (e) {
+				return client.logger.error(`Error trying to fetch role for ${message.author.tag}`, e);
 			}
 		}
 
@@ -68,9 +65,8 @@ module.exports = {
 			try {
 				await member.roles.remove(seregamers, 'Faction quiz role removal');
 				await member.roles.remove(sereaxis, 'Faction quiz role removal');
-			} catch (error) {
-				client.logger.error(`On removeRole() for ${message.author.tag}: ${error}`);
-				return message.reply('Error trying to remove your role.');
+			} catch (e) {
+				return client.logger.error(`Error removing role for ${message.author.tag}`, e);
 			}
 		}
 
@@ -79,7 +75,6 @@ module.exports = {
 
 		let i = 0, points = 0;
 
-		// generate an array of unique number for the question indexes
 		function arrayGenerator() {
 			let arr = [];
 			while (arr.length <= questionsToAsk) {
@@ -92,58 +87,44 @@ module.exports = {
 		let array = arrayGenerator();
 
 		async function askQuestion() {
-			const timeout = '2m';
-			const filter = response => {
-				return quiz[array[i]].answers.some(answer => (answer.toLowerCase() === response.content.toLowerCase()
-					|| (response.content.toLowerCase() === 'abort')) && (response.author.id === message.author.id));
-			};
-
-			let q = await message.channel.send(EmbedGenerator.generate(`${quiz[array[i]].question}`)
-				.setURL(BOT_URL)
-				.addField('Choose your answer:', quiz[array[i]].description)
-				.addField('Awaiting your response...', `This prompt will automatically expire after ${ms(ms(timeout), { long: true })}`)
-				.addField('Hint:', 'Abort the quiz anytime with `abort`'));
-			let answered;
-			message.channel.awaitMessages(filter, { max: 1, time: ms(timeout), errors: ['time'] })
-				.then(async collected => {
-					await q.delete();
-
-					if (collected.first().content === 'abort') return message.channel.send('Quiz aborted.');
-
-					const answerIndex = quiz[array[i]].answers.indexOf(collected.first().content.toLowerCase());
-					points = points + quiz[array[i]].points[answerIndex];
-					answered = true; i++;
-
-					if (i === questionsToAsk) {
-						return saveScore(message.author.id, points);
-					}
-
-					if (answered) askQuestion();
-				})
-				.catch(() => {
-					return q.delete();
-				});
-		}
-
-		async function saveScore(authorUID, score) {
 			try {
-				// equivalent to: INSERT INTO tags (uid, score) values (?, ?);
-				await factionModel.create({
-					uid: authorUID,
-					score: score,
-				});
-				client.logger.log(`Tag with ${score}p added for ${message.author.tag}`);
+				const timeout = '2m';
+
+				let q = await message.channel.send(EmbedGenerator.generate(`${quiz[array[i]].question}`)
+					.setURL(BOT_URL)
+					.addField('Choose your answer:', quiz[array[i]].description)
+					.addField('Awaiting your response...', `This prompt will automatically expire after ${ms(ms(timeout), { long: true })}`)
+					.addField('Hint:', 'Abort the quiz anytime with `abort`'));
+
+				const filter = response => {
+					return quiz[array[i]].answers.some(answer => (answer.toLowerCase() === response.content.toLowerCase()
+						|| (response.content.toLowerCase() === 'abort')) && (response.author.id === message.author.id));
+				};
+
+				let answered = false;
+
+				message.channel.awaitMessages(filter, { max: 1, time: ms(timeout), errors: ['time'] })
+					.then(async collected => {
+						await q.delete();
+
+						if (collected.first().content === 'abort') return message.channel.send('Quiz aborted.');
+
+						const answerIndex = quiz[array[i]].answers.indexOf(collected.first().content.toLowerCase());
+						points = points + quiz[array[i]].points[answerIndex];
+						answered = true; i++;
+
+						if (i === questionsToAsk) {
+							return giveRole();
+						}
+
+						if (answered) askQuestion();
+					})
+					.catch(() => {
+						return q.delete();
+					});
 			} catch (e) {
-				if (e.name === 'SequelizeUniqueConstraintError') {
-					let affectedRows = await factionModel.update({ score: points }, { where: { uid: authorUID } });
-					if (affectedRows > 0) {
-						client.logger.log(`Tag with ${score} points updated for ${message.author.tag}`);
-					} else {
-						client.logger.error(`Could not find tag for ${message.author.tag}`);
-					}
-				}
+				return client.logger.error(e);
 			}
-			giveRole();
 		}
 
 		async function giveRole() {
@@ -151,26 +132,24 @@ module.exports = {
 				switch (true) {
 					case points >= 50:
 						await member.roles.add(seregamers, 'Obtained from Faction quiz');
-						await message.channel.send('Congrats! You have been awarded `SereGamers` role.');
-						client.logger.log(`${message.author.tag} obtained SereGamers role`);
+						await message.channel.send(`Congrats! You have been awarded **${seregamers.name}** role.`);
 						break;
 					case points <= -50:
 						await member.roles.add(sereaxis, 'Obtained from Faction quiz');
-						await message.channel.send('Congrats! You have been awarded `SereAxis` role.');
-						client.logger.log(`${message.author.tag} obtained SereAxis role`);
+						await message.channel.send(`Congrats! You have been awarded **${sereaxis.name}** role.`);
 						break;
 					case points < 50 && points > -50:
 						client.logger.log(`${message.author.tag} is faction neutral`);
-						message.channel.send('Seems like you live in between of both factions.\nPerfectly balanced, as all things should be.');
+						message.channel.send('You prefer to live in both factions, thus no role has been assigned.');
 						break;
 					default:
 						client.logger.error(`There's something wrong with ${message.author.tag}'s points: ${points}`);
 						message.channel.send('Something went wrong calculating your score.\nPlease tell my owner about this.');
 						break;
 				}
-			} catch (error) {
-				client.logger.error(`Error executing giveRole() for ${message.author.tag}: ${error}`);
-				message.channel.send('Error trying to give faction role.');
+			} catch (e) {
+				client.logger.error(`Error giving role for ${message.author.tag}`, e);
+				return message.channel.send('Error trying to give faction role.');
 			}
 		}
 
